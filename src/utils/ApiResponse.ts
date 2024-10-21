65533d14b801289ba4156abbbd77cb16fff26522
info-higaurav <ai.higaurav@gmail.com>
2024-10-21 09:39:52 +0530

    ApiResponse class added

---
+++ src/utils/ApiResponse.ts
@@ -0,0 +1,28 @@
+import { Response } from "express";
+
+class ApiResponse {
+    data : [];
+    message : string;
+    statusCode : number;
+
+    constructor(data : [] , message : string , statusCode : number){
+        this.data=data;
+        this.message=message;
+        this.statusCode=statusCode;
+    }
+
+    static success (data : [] , message : string , statusCode : number ){
+        return new ApiResponse (data , message , statusCode)
+    }
+
+    static failure (data : [] , message : string , statusCode : number ){
+        return new ApiResponse (data , message , statusCode)
+    }
+
+    send (res : Response){
+        res.status(this.statusCode).json(this)
+    }
+
+}
+
+export default ApiResponse;
