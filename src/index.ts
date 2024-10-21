6290d8b7731727517c36a13b6756f0fcb6b0c950
info-higaurav <ai.higaurav@gmail.com>
2024-10-21 11:21:17 +0530

    user route added

--- src/index.ts
+++ src/index.ts
@@ -6,12 +6,22 @@ dotenv.config({
     path:"./.env"
 })
 
+// import routes
+import user from './routes/user.route'
+
+// config routes
+app.use('/api/v1/users', user)
+
+// connect to database
 connectToDb()
+
+// start server
 .then(()=>{
     app.listen(process.env.PORT || 3000 ,()=>{
         console.log("server Status : OK")
     })
 })
+
 .catch((err)=>{
     console.log("Mongodb Connection failed " + err)
     process.exit(1)
